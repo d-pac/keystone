43f911f190e097d3a7eca553356c7eef1b41ad2f
Camille Reynders <camille.reynders@gmail.com>
2017-03-30 16:35:02 +0200

    Fix datetime field

--- fields/types/datetime/DatetimeType.js
+++ fields/types/datetime/DatetimeType.js
@@ -4,7 +4,8 @@ var FieldType = require('../Type');
 var util = require('util');
 var _ = require('underscore');
 
-var parseFormats = ['YYYY-MM-DD', 'YYYY-MM-DD h:m:s a', 'YYYY-MM-DD h:m a', 'YYYY-MM-DD H:m:s', 'YYYY-MM-DD H:m'];
+// ISO_8601 is needed for the automatically created createdAt and updatedAt fields
+var parseFormats = ['YYYY-MM-DD', 'YYYY-MM-DD h:m:s a', 'YYYY-MM-DD h:m a', 'YYYY-MM-DD H:m:s', 'YYYY-MM-DD H:m', 'YYYY-MM-DD h:mm:s a Z', moment.ISO_8601];
 
 /**
  * DateTime FieldType Constructor
@@ -19,6 +20,7 @@ function datetime(list, path, options) {
 	this.typeDescription = 'date and time';
 	this.parseFormatString = options.parseFormat || parseFormats;
 	this.formatString = (options.format === false) ? false : (options.format || 'YYYY-MM-DD h:mm:ss a');
+	this.tzFormatString = 'Z';
 
 	// Create an array of moment time format characters to help find where the time portion of the format string beings
 	var timeOptions = ['h', 'H', 'm', 's', 'S'];
@@ -48,14 +50,14 @@ function datetime(list, path, options) {
 		this.timePlaceholder = '';
 	}
 
-	this.isUTC = options.utc || false;
 	if (this.formatString && 'string' !== typeof this.formatString) {
 		throw new Error('FieldType.DateTime: options.format must be a string.');
 	}
 	datetime.super_.call(this, list, path, options);
 	this.paths = {
 		date: this._path.append('_date'),
-		time: this._path.append('_time')
+		time: this._path.append('_time'),
+		tzOffset: this._path.append('_tzOffset'),
 	};
 }
 util.inherits(datetime, FieldType);
@@ -70,11 +72,14 @@ datetime.prototype.parse = DateType.prototype.parse;
  * Get the value from a data object; may be simple or a pair of fields
  */
 datetime.prototype.getInputFromData = function(data) {
-	if (this.paths.date in data && this.paths.time in data) {
-		return (data[this.paths.date] + ' ' + data[this.paths.time]).trim();
-	} else {
-		return data[this.path];
+	var dateValue = data[this.paths.date];
+	var timeValue = data[this.paths.time];
+	var tzOffsetValue = data[this.paths.tzOffset];
+	if (dateValue && timeValue) {
+		return dateValue.trim() + ' ' + timeValue.trim() + ' ' + tzOffsetValue;
 	}
+
+	return data[this.path];
 };
 
 /**
@@ -82,33 +87,33 @@ datetime.prototype.getInputFromData = function(data) {
  * An empty value clears the stored value and is considered valid
  */
 datetime.prototype.validateInput = function(data, required, item) {
-	if (!(this.path in data && !(this.paths.date in data && this.paths.time in data)) && item && item.get(this.path)) return true;
-	var newValue = moment(this.getInputFromData(data), parseFormats);
-	if (required && (!newValue || !newValue.isValid())) {
+	var value = this.getInputFromData(data);
+	
+	if(!!value){
+		return moment(value, parseFormats).isValid();
+	}else if(required){
 		return false;
-	} else if (this.getInputFromData(data) && newValue && !newValue.isValid()) {
-		return false;
-	} else {
-		return true;
 	}
+	
+	return true;
+
 };
 
 /**
  * Updates the value for this field in the item from a data object
  */
 datetime.prototype.updateItem = function(item, data) {
-	if (!(this.path in data || (this.paths.date in data && this.paths.time in data))) {
-		return;
-	}
-	var m = this.isUTC ? moment.utc : moment;
-	var newValue = m(this.getInputFromData(data), this.formatString);
-	if (newValue.isValid()) {
-		if (!item.get(this.path) || !newValue.isSame(item.get(this.path))) {
-			item.set(this.path, newValue.toDate());
+	var value = this.getInputFromData(data);
+	var oldValue = item.get(this.path);
+	if(value){
+		var mValue = moment(value, this.formatString + ' ' + this.tzFormatString).utc();
+		if(!mValue.isSame(oldValue)){
+			item.set(this.path, mValue.toDate());
 		}
-	} else if (item.get(this.path)) {
+	}else if(oldValue){
 		item.set(this.path, null);
 	}
+	
 };
 
 /* Export Field Type */
